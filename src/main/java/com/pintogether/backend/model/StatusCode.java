07598fcb0b61415ae30850e47c104a579a118e71
eunjilee <dhbdg11@gmail.com>
2024-03-12 11:55:59 +0900

    Feat: ErrorCode-> StatusCode로 이름 수정 및 status Code 추가

--- src/main/java/com/pintogether/backend/model/StatusCode.java
+++ src/main/java/com/pintogether/backend/model/StatusCode.java
@@ -3,13 +3,15 @@ package com.pintogether.backend.model;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-public enum ErrorCode {
+public enum StatusCode {
     OK(200, "OK", HttpStatus.OK),
     BAD_REQUEST(400, "BAD REQUEST", HttpStatus.OK),
 
     CREATED(201, "CREATED", HttpStatus.CREATED),
 
-    NO_CONTENT(204, "NO_CONTENT", HttpStatus.NO_CONTENT)
+    NO_CONTENT(204, "NO_CONTENT", HttpStatus.NO_CONTENT),
+
+    NOT_FOUND(404, "NOT_FOUND", HttpStatus.NOT_FOUND)
     ;
 
     @Getter
@@ -19,7 +21,7 @@ public enum ErrorCode {
     @Getter
     private final HttpStatus httpStatus;
 
-    ErrorCode(int code, String message, HttpStatus httpStatus) {
+    StatusCode(int code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
         this.httpStatus = httpStatus;
