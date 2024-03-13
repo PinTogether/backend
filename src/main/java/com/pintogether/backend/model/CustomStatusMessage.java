6989ae0a0af6ec8375e2f3120f28e5536a89a795
eunjilee <dhbdg11@gmail.com>
2024-03-14 01:52:24 +0900

    Feat: Collection not found 이넘 타입 추가

--- src/main/java/com/pintogether/backend/model/CustomStatusMessage.java
+++ src/main/java/com/pintogether/backend/model/CustomStatusMessage.java
@@ -3,7 +3,8 @@ package com.pintogether.backend.model;
 import lombok.Getter;
 
 public enum CustomStatusMessage {
-    MEMBER_NOT_FOUND("존재하지 않는 멤버입니다.");
+    MEMBER_NOT_FOUND("존재하지 않는 멤버입니다."),
+    COLLECTION_NOT_FOUND("존재하지 않는 컬렉션입니다.");
 
     @Getter
     private final String message;
