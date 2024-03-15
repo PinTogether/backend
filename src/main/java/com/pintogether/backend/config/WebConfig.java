0ec5c4585e2c4a926ee8b06f1958ad44306abc4b
eunjilee <dhbdg11@gmail.com>
2024-03-15 17:35:00 +0900

    Feat: collectionId, commentId 경로변수를 객체화하는 resolver 구현

---
+++ src/main/java/com/pintogether/backend/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.pintogether.backend.config;
+
+
+import com.pintogether.backend.handlerMethodArgumentResolver.CollectionArgumentResolver;
+import com.pintogether.backend.handlerMethodArgumentResolver.CollectionCommentArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final CollectionArgumentResolver collectionArgumentResolver;
+    private final CollectionCommentArgumentResolver collectionCommentArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(collectionArgumentResolver);
+        resolvers.add(collectionCommentArgumentResolver);
+    }
+}
