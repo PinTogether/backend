c1232f48d79273e3ceb921b5165793f12104c2c4
eunjilee <dhbdg11@gmail.com>
2024-04-13 11:05:48 +0900

    fix: 멤버 검색 비회원 허용

--- src/main/java/com/pintogether/backend/config/SecurityConfig.java
+++ src/main/java/com/pintogether/backend/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig {
                                     new AntPathRequestMatcher("/places**"),
                                     new AntPathRequestMatcher("/places/**"),
                                     new AntPathRequestMatcher("/search/collections**"),
-                                    new AntPathRequestMatcher("/search/places**")
+                                    new AntPathRequestMatcher("/search/places**"),
+                                    new AntPathRequestMatcher("/search/members**")
                             )
                                 .permitAll()
                             .anyRequest()
